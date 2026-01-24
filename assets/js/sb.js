f7cd32453d40fcd6f484a121107f472963546a9d
satv2025 <traplegionrecords2020@gmail.com>
2026-01-24 20:18:15 -0300

    ACT

---
+++ assets/js/sb.js
@@ -0,0 +1,4 @@
+window.sb = window.supabase.createClient(
+    "https://moacgzwrvegpoatwjqto.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWNnendydmVncG9hdHdqcXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyOTM5MDQsImV4cCI6MjA4NDg2OTkwNH0.WGzWhO1KYKTXKLaIXIpE1OLs6o4BJK-Q4JLK_RPZkGM"
+);
