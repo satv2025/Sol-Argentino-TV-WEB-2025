2a68a685fe5c70a7deaa2c3024306817ae0fcff1
satv2025 <traplegionrecords2020@gmail.com>
2026-01-31 02:22:20 -0300

    act

--- assets/js/sb.js
+++ assets/js/sb.js
@@ -1,4 +1,4 @@
 window.sb = window.supabase.createClient(
-    "https://spzgxvkedsdjjhzzyysr.supabase.co",
+    "https://api.solargentinotv.com.ar",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwemd4dmtlZHNkampoenp5eXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MzQwOTAsImV4cCI6MjA4NTExMDA5MH0.RgFghlZVV4Ww27rfh96nTiafDwRu9jtC3S6Y6aFdIxE"
 );
